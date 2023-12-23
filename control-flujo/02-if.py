a930fb3a1529cc09aba6e3b957463095c4835cfe
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-12-23 16:23:12 -0300

    if,elif,else

---
+++ control-flujo/02-if.py
@@ -0,0 +1,10 @@
+edad = 55
+
+if edad > 54:
+    print("Descuentaso")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes ver la peli")
+    print("ve a otro lado")
+print("Listo")
